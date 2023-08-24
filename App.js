67dd9aa9efc2f9fad12d8bdb41a44ec47ea77dd2
Agu Christopher <aguchris740@gmail.com>
2023-08-24 18:38:32 +0100

    started project

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text>
+       */}
+      <StatusBar style="light" />
+      <Image
+        source={require("./assets/LOGO.png")}
+        style={{ width: 120, height: 150 }}
+        resizeMode="contain"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#121212",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
