6612b466991f456dcb9123b3cfbf16766b36f9d0
Agu Christopher <aguchris740@gmail.com>
2023-08-28 20:43:25 +0100

    Added V-Keyboard

---
+++ components/vkeyboard/VirtualKeyboard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import NumberButton from "./NumberButton";
+
+const VirtualKeyboard = ({ onNumberPress }) => {
+  const numberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  return (
+    <View style={styles.keyboard}>
+      {numberButtons.map((number) => (
+        <NumberButton key={number} number={number} onPress={onNumberPress} />
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  keyboard: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+});
+
+export default VirtualKeyboard;
